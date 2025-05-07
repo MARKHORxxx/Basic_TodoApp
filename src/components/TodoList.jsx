e00cd5bc3e8c0c8010af211c144928ae65b63a09
MARKHORxxx <146568913+MARKHORxxx@users.noreply.github.com>
2025-05-08 00:33:36 +0500

    Add files via upload

---
+++ src/components/TodoList.jsx
@@ -0,0 +1,55 @@
+import { useState } from "react"
+
+function TodoList(){
+
+    const [task,SetTask]=useState('')
+    const [data,SetData]=useState([])
+
+    const AddTask=(e)=>{
+        e.preventDefault()
+        if(task.trim()){
+            SetData([...data,{id:Date.now(),name:task,completed:false}])
+            SetTask('')
+            console.log(data);
+
+        }
+    }
+    
+     const toggleTask=(id)=>{
+        SetData(data.map((t)=>t.id===id?{...t,completed: !t.completed} :t))
+     }
+
+
+    return(<>
+    <h1>Todo List</h1>
+    <form onSubmit={AddTask}>
+        <input type="text" value={task} onChange={(e)=>SetTask(e.target.value)} />
+        <button type="submit">Add Task</button>
+
+    </form>
+    
+    <ul>{
+        data.map((t)=>{
+            return(
+                <>
+                <li key={t.id}
+                style={{color: t.completed? 'red':'green'}}
+                >
+                    {t.name}
+                    <button onClick={()=>toggleTask(t.id)}>
+                        {t.completed ?'Undo':'complete'}
+                    </button>
+                    </li>
+                </>
+            )
+                
+
+            
+
+        })}
+        </ul>
+    </>)
+        
+}
+
+export default TodoList
